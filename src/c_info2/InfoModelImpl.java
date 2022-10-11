070e05c8039172244b946aea3038d234c0ae045c
nononoh87 <112688283+nononoh87@users.noreply.github.com>
2022-10-11 16:40:03 +0900

    "JDBC - 2022.10.11"

---
+++ src/c_info2/InfoModelImpl.java
@@ -0,0 +1,145 @@
+package c_info2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class InfoModelImpl implements InfoModel {
+
+	// final로 선언한 경우 변수 부분 대문자로 표기
+	final static String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	final static String URL = "jdbc:oracle:thin:@127.0.0.1:1521:xe"; // 127.0.0.1 : 본인 컴퓨터
+	final static String USER = "scott";
+	final static String PASS = "tiger";
+
+	public InfoModelImpl() throws ClassNotFoundException {
+		// 1. 드라이버 로딩
+		Class.forName(DRIVER);
+		System.out.println("드라이버로딩 성공");
+	}
+
+	/*
+	 * 사용자 입력값을 받아서 DB에 저장하는 역할
+	 */
+	@Override
+	public void insertInfo(InfoVO vo) throws SQLException {
+		// 2. 연결객체 얻어오기
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+
+			// 3. sql 문장 (#)
+			String sql = " INSERT INTO info_tab (name, jumin, tel, gender, age, home)      " + " VALUES(?,?,?,?,?,?)";
+
+			// 4. 전송객체
+			ps = con.prepareStatement(sql);
+
+			// 세팅 - #
+			ps.setString(1, vo.getName());
+			ps.setString(2, vo.getId());
+			ps.setString(3, vo.getTel());
+			ps.setString(4, vo.getGender());
+			ps.setInt(5, vo.getAge());
+			ps.setString(6, vo.getHome());
+
+			// 5. 전송
+			ps.executeUpdate();
+
+		} finally {
+			// 6. 닫기 (필수) : Connection 의 갯수는 사용하는 사용자의 수보다
+			ps.close();
+			con.close();
+		}
+	} // end of insertInfo()
+	/*
+	 * 전체 info_tab의 레코드 검색
+	 */
+
+	@Override
+	public ArrayList<InfoVO> selectAll() throws SQLException {
+		// 2. 연결객체 얻어오기
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+
+			// 3. SQL 문장
+			String sql = "SELECT* FROM info_tab";
+
+			// 4. 전송객체 얻어오기
+			ps = con.prepareStatement(sql);
+
+			// 5. 전송
+			rs = ps.executeQuery();
+
+			ArrayList<InfoVO> list = new ArrayList<InfoVO>();
+			while (rs.next()) {
+				InfoVO vo = new InfoVO();
+				vo.setName(rs.getString("NAME"));
+				vo.setId(rs.getString("JUMIN"));
+				vo.setTel(rs.getString("TEL"));
+				vo.setGender(rs.getString("GENDER"));
+				vo.setAge(rs.getInt("AGE"));
+				vo.setHome(rs.getString("HOME"));
+
+				list.add(vo);
+			}
+			return list;
+
+		} finally {
+			// 6. 닫기 (필수) : Connection 의 갯수는 사용하는 사용자의 수보다
+			rs.close();
+			ps.close();
+			con.close();
+		}
+	} // end of selectInfo()
+
+	public InfoVO selectByTel(String tel) throws SQLException {
+		// 2. 연결객체 얻어오기
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		InfoVO vo = new InfoVO();
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+			// 3.sql 문장
+			String sql = "SELECT * FROM info_tab WHERE tel=?";
+			// 4. 정송객체 얻어오기
+			ps = con.prepareStatement(sql);
+			ps.setString(1, tel);
+			// 5. 전송
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				vo.setName(rs.getString("name"));
+				vo.setId(rs.getString("jumin"));
+				vo.setTel(rs.getString("tel"));
+				vo.setGender(rs.getString("gender"));
+				vo.setAge(rs.getInt("age"));
+				vo.setHome(rs.getString("home"));
+			}
+
+		} finally {
+			// 6. 닫기 (필수) : Connection 의 갯수는 사용하는 사용자의 수보다
+
+			ps.close();
+			con.close();
+		}
+		return vo;
+	} // end of selectByTel
+
+	/*
+	 * 메소드명 : delete 인자 : 전화번호 리턴값 : 삭제한 행 수 역할 : 전화번호를 넘겨받아 해당 전화번호의 레코드 삭제
+	 */
+
+	@Override
+	public int delete(String tel) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}// infoModelImpl
